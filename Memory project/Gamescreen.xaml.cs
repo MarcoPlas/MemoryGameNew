f6c76392af8f82dc01e11a58c1adc4eefc9c0ac6
FietsPomp <marco.plas@student.nhlstenden.com>
2020-10-18 14:34:19 +0200

    Nog maal eens op hoop van zege. Met deze week de tekst. Wees standvastig in de heer. U mag thuis deze tekst bestuderen. Al zullen er duizend vallen aan uw zijde en tienduizend aan uw rechterhand – bij u zal het onheil niet komen. (Psalmen 91:7)

--- Memory project/Gamescreen.xaml.cs
+++ Memory project/Gamescreen.xaml.cs
@@ -20,13 +20,78 @@ namespace Memory_project
 	/// </summary>
 	public partial class Gamescreen : Page
 	{
-		public Gamescreen()
-		{
-			InitializeComponent();
-		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        Random rnd = new Random();
+
+        public Gamescreen()
         {
+            InitializeComponent();
+            AddImages();
+            MyButton.FontSize = 22;
+            Grid.SetColumn(MyButton, 7);
+        }
+
+        int turns = 0;
+
+        public void AddImages()
+        {
+            List<ImageSource> images = GetImageList();
+            for (int r = 1; r <= 4; r++)
+            {
+                for (int c = 2; c < 6; c++)
+                {
+
+                    Image BackgroundImage = new Image();
+                    Uri path = new Uri("Images/Backside.png", UriKind.Relative);
+                    BackgroundImage.Source = new BitmapImage(path);
+                    BackgroundImage.Margin = new Thickness(4);
+                    BackgroundImage.Tag = images.First();
+                    images.RemoveAt(0);
+                    BackgroundImage.MouseDown += new MouseButtonEventHandler(TurnCard);
+                    Grid.SetColumn(BackgroundImage, c);
+                    Grid.SetRow(BackgroundImage, r);
+                    GameGrid.Children.Add(BackgroundImage);
+                }
+            }
+        }
+
+        //TODO: Randomise Kaarten
+        //TODO: Kaarten weer terug draaien
+        //TODO: Een 6x6 veld en 8x8 veld
+
+
+        public void TurnCard(object sender, MouseButtonEventArgs e)
+        {
+            turns++;
+            Image card = (Image)sender;
+            ImageSource front = (ImageSource)card.Tag;
+            card.Source = front;
+            if (turns == 2)
+            {
+                MessageBox.Show(Convert.ToString(turns));
+                turns -= 2;
+            }
+        }
+
+        public List<ImageSource> GetImageList()
+        {
+            List<ImageSource> result = new List<ImageSource>();
+
+            for (int i = 0; i < 16; i++)
+            {
+                int nr = i % 8 + 1;
+                Uri path = new Uri("Images/" + nr + ".png", UriKind.Relative);
+                result.Add(new BitmapImage(path));
+
+            }
+            return result;
+        }
+
+
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hoi");
 
         }
     }
